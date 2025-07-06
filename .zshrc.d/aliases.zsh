ebaf5593714befc7991f7f511f05f3d5cb5ebf2f
Samuel Wu <twopizza9621536@gmail.com>
2025-07-06 16:54:55 -0400

    feat: 🎉 Move zsh config to own repo again
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

---
+++ .zshrc.d/aliases.zsh
@@ -0,0 +1,70 @@
+#!/usr/bin/env zsh
+# SPDX-FileCopyrightText: 2024 Samuel Wu
+# SPDX-FileCopyrightText: 2023 mattmc3
+#
+# SPDX-License-Identifier: MIT
+#
+# .aliases - Set whatever shell aliases you want.
+#
+
+# mask built-ins with better defaults
+alias vi=vim
+
+# fix common typos
+alias quit=exit
+alias cls=clear
+
+# aliases belak/zsh-utils
+alias type="type -a"
+alias mkdir="mkdir -p"
+
+# ls
+alias ls='eza -F --time-style=long-iso --group-directories-first --git --icons --color-scale all'
+alias l=ls
+alias ll='ls -lh'
+
+alias la='ll -a'
+alias lD='ll -D'
+alias lc='ll -1'
+alias lh='ll -d .*'
+
+alias lsa='la -a'
+alias lx='lsa -gHimUuS'
+alias lxa='lx -Z@'
+
+alias lt='ls -T'
+alias tree=lt
+
+# find
+alias find=fd
+alias fD='find -t d'
+alias fF='find -t f'
+
+# diff
+alias diff=delta
+
+# grep
+grep() {
+  rg "$@" | diff
+}
+
+# tar
+alias tar='bsdtar --posix'
+alias tarls='bsdtar -tvf'
+alias untar='bsdtar -xvf'
+
+# top
+alias top=btop
+
+# xxd
+alias xxd=hexyl
+
+# print paths and functions
+alias print-fpath='for fp in $fpath; do printf "%s\n" "$fp"; done; unset fp'
+alias print-path='printf "%s\n" "$PATH" | tr ":" "\n"'
+alias print-functions='print -l ${(k)functions[(I)[^_]*]} | sort'
+
+# misc
+alias zshrc='${EDITOR} ${ZDOTDIR}/.zshrc'
+alias zbench='for i in {1..10}; do /usr/bin/time zsh -lic exit; done'
+alias zdot='cd ${ZDOTDIR}'
