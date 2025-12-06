f2cca8616f243acf88d2091973591a45f8704f33
Samuel Wu (吴申榕) <twopizza9621536@gmail.com>
2025-12-05 23:04:31 -0500

    Update private_aliases.zsh
    Signed-off-by: Samuel Wu (吴申榕) <twopizza9621536@gmail.com>

--- private_conf.d/private_aliases.zsh
+++ private_conf.d/private_aliases.zsh
@@ -54,6 +54,7 @@ alias print-path='printf "%s\n" "${PATH}" | tr ":" "\n"'
 alias print-functions='print -l ${(k)functions[(I)[^_]*]} | sort'
 
 # misc
+alias colormap='for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+"\n"}; done'
 alias zshrc='${EDITOR} ${ZDOTDIR}/.zshrc'
 alias zbench='for i in {1..10}; do /usr/bin/time zsh -lic exit; done'
 alias zdot='cd ${ZDOTDIR}'
