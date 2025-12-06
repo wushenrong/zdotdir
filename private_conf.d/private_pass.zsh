7d9a4614160d55a8bc2b5f50985f9fb681c123d3
Samuel Wu (吴申榕) <twopizza9621536@gmail.com>
2025-12-05 22:56:17 -0500

    Update private_pass.zsh
    Signed-off-by: Samuel Wu (吴申榕) <twopizza9621536@gmail.com>

--- private_conf.d/private_pass.zsh
+++ private_conf.d/private_pass.zsh
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: MIT
 
 if [[ -n $TTY ]]; then
-  export GPG_TTY="$TTY"
+  export GPG_TTY="${TTY}"
 else
   export GPG_TTY="$(tty)"
 fi
 
 if [[ -n $WSLENV ]]; then
-  export SSH_SK_HELPER="/mnt/c/Program Files/OpenSSH/ssh-sk-helper.exe"
+  export SSH_SK_HELPER='/mnt/c/Program Files/OpenSSH/ssh-sk-helper.exe'
 fi
