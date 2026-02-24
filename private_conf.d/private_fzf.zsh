942119ab2876657fde8218ebf9bb4428b6f75687
Samuel Wu (吴申榕) <twopizza9621536@gmail.com>
2026-02-23 23:28:47 -0500

    Update private_fzf.zsh
    Signed-off-by: Samuel Wu (吴申榕) <twopizza9621536@gmail.com>

--- private_conf.d/private_fzf.zsh
+++ private_conf.d/private_fzf.zsh
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-export FZF_DEFAULT_COMMAND="bfs -type f -mindepth 1 -unique -L -exclude -name .git -printf '%P\n'"
+export FZF_DEFAULT_COMMAND="bfs -type f -mindepth 1 -nohidden -printf '%P\n'"
 export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND} -color"
 export FZF_ALT_C_COMMAND="bfs -type d -mindepth 1 -nohidden -printf '%P\n'"
 
