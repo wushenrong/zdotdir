fb1c68c06a4bb034625c76aade3d2862cc0116fc
Samuel Wu <twopizza9621536@gmail.com>
2026-08-23 22:09:09 -0400

    feat: Add zsh-bench and angle brackets for autopair
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- conf.d/30_plugins.zsh
+++ conf.d/30_plugins.zsh
@@ -10,16 +10,17 @@ zsh-defer source "$_plugin_location/fzf-tab/fzf-tab.plugin.zsh"
 source "$_plugin_location/powerlevel10k/powerlevel10k.zsh-theme"
 
 # Utilties
-# romkatv/zsh-bench kind:path
+path+=("$_plugin_location/zsh-bench")
 
 # Other Fish-like features
+typeset -gA AUTOPAIR_PAIRS
+AUTOPAIR_PAIRS+=("<" ">")
+
 source "$_plugin_location/zsh-autopair/zsh-autopair.plugin.zsh"
 autopair-init
 
 zsh-defer source "$_plugin_location/fsh/fast-syntax-highlighting.plugin.zsh"
 zsh-defer source "$_plugin_location/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
 zsh-defer source "$_plugin_location/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
-
-# Completions
 
 unset _plugin_location
