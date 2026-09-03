d59fa31ab0e7ea3ca4f26bca94eee1f6629bd946
Samuel Wu <twopizza9621536@gmail.com>
2026-09-03 00:06:01 -0400

    feat: Update variables and aliases
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- conf.d/40_aliases.zsh
+++ conf.d/40_aliases.zsh
@@ -6,22 +6,6 @@ alias cls=clear
 alias type='type -a'
 alias mkdir='mkdir -p'
 
-# Use XDG for wget
-alias wget="wget --hsts-file=$XDG_STATE_HOME/wget-hsts"
-
-# Set history aliases.
-alias hist='fc -li'
-alias histsync='fc -RI'
-alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
-
-# print paths and functions
-alias print-fpath='for fp in $fpath; do printf "%s\n" "$fp"; done; unset fp'
-alias print-path='printf "%s\n" "${PATH}" | tr ":" "\n"'
-alias print-functions='print -l ${(k)functions[(I)[^_]*]} | sort'
-
-# Print a simple colormap.
-alias colormap='for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+"\n"}; done'
-
 # Kitty terminal commands
 if [[ "$TERM" == xterm-kitty ]]; then
   alias icat='kitten icat'
